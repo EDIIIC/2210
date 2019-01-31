dd8156ea8bf9e1363f6c9000b37cdb4ae8130044
Ethan Cragun <craguned@gmail.com>
2019-01-31 11:57:28 -0700

    Started Egout Prototype

---
+++ GAme Design/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour 
+{
+/*    
+    public static int PlayerCount;
+    public static int PlatformCount;
+
+    public AHighScores_SO highScoresSo;
+    public GameObject scoreboard;
+*/
+    
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.Return)) 
+        {
+            SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+    
+    public void LevelLoad (string lvl) 
+    {
+        SceneManager.LoadScene (lvl);
+    }    
+     
+    public void ExitGame ()
+    {
+        print ("GET OUT'A HERE");
+//        highScoresSo.highScores.Add(ScoreBoard.scoreValue);
+//        highScoresSo.OrderScores();
+        Application.Quit ();
+    }
+     
+    public void LoadNextLevel ()
+    {
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+//        playerscore = 0;
+    }
+    
+}
+
